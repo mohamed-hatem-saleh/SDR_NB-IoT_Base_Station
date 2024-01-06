abfc36917bb34edd64c2929aec4b8e5980ab3356
AbdulRahmanBenatia <95504237+AbdulRahmanBenatia@users.noreply.github.com>
2024-01-06 23:59:56 +0200

    Done.

--- Plot_Spectrum.m
+++ Plot_Spectrum.m
@@ -1,11 +1,15 @@
-function Plot_Spectrum(sig,fs)
+function Plot_Spectrum(sig,fs,lim)
 
 [signal,f] = pwelch(sig,2048,1024,2048,fs);
+% [signal] = fft(sig);
 signal = fftshift(signal/max(abs(signal)));
 F_new = linspace(-fs/2,fs/2,length(signal));
 plot(F_new/1e3,(20*log10(signal)));
 xlabel('Frequency in (KHz)')
 ylabel('Normalized Magnitude (dB)')
 grid on;
+if(lim==1)
+    xlim([-1000,1000]);
+end
 
 end
